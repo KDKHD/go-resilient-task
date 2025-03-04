9d0172f54f25ccb3487ee331f42b48ff735cf1d6
Kenneth Kreindler <kenneth.kreindler@elastic.co>
2025-03-04 23:30:30 +0000

    Update readme

--- example/uber-fx/resilient-task-configuration.go
+++ example/uber-fx/resilient-task-configuration.go
@@ -44,22 +44,21 @@ func NewResilientTaskConfiguration(logger *zap.Logger, postgresClient *sql.DB, k
 }
 
 func PaymentInitiatedHandler(logger *zap.Logger, config *config.GoResilientTaskConfig) taskhandler.ITaskHandler {
-	return taskhandleradapter.
-		NewTaskHandlerAdapterBuilder(
-			func(task taskmodel.IBaseTask) bool {
-				return task.GetType() == "payment_initiated"
+	return taskhandleradapter.NewTaskHandlerAdapterBuilder(
+		func(task taskmodel.IBaseTask) bool {
+			return task.GetType() == "payment_initiated"
+		},
+		taskprocessor.TaskProcessorFunc(
+			func(task taskmodel.ITask) (taskprocessor.ProcessResult, error) {
+				logger.Debug("processing task payment initaiated", zap.String("task_data", task.GetData()))
+				logger.Debug("Triggering task payment_processed", zap.String("task_data", task.GetData()))
+				config.GetTaskService().AddTask(taskservice.AddTaskRequest{Type: "payment_processed", TaskId: uuid.New(), Data: []byte(task.GetData()), RunAfterTime: time.Now().UTC().Add(time.Second * 10), ExpectedQueueTime: time.Second * 120})
+				return taskprocessor.ProcessResult{
+					ResultCode: taskprocessor.DONE,
+				}, nil
 			},
-			taskprocessor.TaskProcessorFunc(
-				func(task taskmodel.ITask) (taskprocessor.ProcessResult, error) {
-					logger.Debug("processing task payment initaiated", zap.String("task_data", task.GetData()))
-					logger.Debug("Triggering task payment_processed", zap.String("task_data", task.GetData()))
-					config.GetTaskService().AddTask(taskservice.AddTaskRequest{Type: "payment_processed", TaskId: uuid.New(), Data: []byte(task.GetData()), RunAfterTime: time.Now().UTC().Add(time.Second * 10), ExpectedQueueTime: time.Second * 120})
-					return taskprocessor.ProcessResult{
-						ResultCode: taskprocessor.DONE,
-					}, nil
-				},
-			),
-		).
+		),
+	).
 		WithConcurrencyPolicy(
 			concurrencypolicy.NewSimpleTaskConcurrencyPolicy(20, logger),
 		).
